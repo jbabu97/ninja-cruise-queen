7794d35bbc303eba4e2f09a15265889c5e42c14c
Jabbar Babu <jabbar.babu81@gmail.com>
2021-01-30 02:33:35 +0600

    adde greetings message for booking

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const firstClassIncrement = document.getElementById('first_increment_btn');
 firstClassIncrement.addEventListener('click', function () {
     ticketCountUpdate('first_class', true);  // call ticketCountUpdate function
-    ticketFareCalculate()
+    ticketFareCalculate();
     // const inputTicket = document.getElementById('first_class');
     // const ticketCount = parseInt(inputTicket.value);
     // const newTicketCount = ticketCount + 1;
@@ -14,7 +14,7 @@ firstClassIncrement.addEventListener('click', function () {
 const firstClassDecrement = document.getElementById('first_decrement_btn');
 firstClassDecrement.addEventListener('click', function () {
     ticketCountUpdate('first_class', false);  // call ticketCountUpdate function
-    ticketFareCalculate()
+    ticketFareCalculate();
     // const inputTicket = document.getElementById('first_class');
     // const ticketCount = parseInt(inputTicket.value);
     // const newTicketCount = ticketCount - 1;
@@ -26,13 +26,13 @@ firstClassDecrement.addEventListener('click', function () {
 const economyClassIncrement = document.getElementById('economy_increment_btn');
 economyClassIncrement.addEventListener('click', function () {
     ticketCountUpdate('economy_class', true);  // call ticketCountUpdate function
-    ticketFareCalculate()
+    ticketFareCalculate();
 });
 
 const economyClassDecrement = document.getElementById('economy_decrement_btn');
 economyClassDecrement.addEventListener('click', function () {
     ticketCountUpdate('economy_class', false);  // call ticketCountUpdate function
-    ticketFareCalculate()
+    ticketFareCalculate();
 });
 
 // function for ticket count update
@@ -69,7 +69,7 @@ function ticketFareCalculate() {
     const totalTicketFare = firstClassTicketCount * firstClassFare + economyClassTicketCount * economyClassFare;
     document.getElementById('ticket_fare').innerText = totalTicketFare;
     // calculate tax amount
-    const taxAmount = totalTicketFare * .1;
+    const taxAmount = Math.round(totalTicketFare * .1);
     document.getElementById('tax_amount').innerText = taxAmount;
     // calculate total fare
     const grandTotal = totalTicketFare + taxAmount;
@@ -84,6 +84,13 @@ function getTicketInput(id) {
     const ticketCount = parseInt(inputTicket.value);
 
     return ticketCount;
-}
+};
+
+//  greetings for booking
+
+const booking = document.getElementById('booking').addEventListener('click', function () {
+    const greetings = document.getElementById('greetings');
+    greetings.style.display = 'block'
+});
 
 
